4fe2c0693558a031e7aff552dc6efdcd9f0ed443
Jacek Popiel <jacekpopiel@protonmail.com>
2024-10-30 13:12:23 +0100

    Create errorMessages.da.jsx

---
+++ errorMessages.da.jsx
@@ -0,0 +1,5 @@
+const translations = {
+	"error.reason.EvaluationDateInFutureError": "Det er ikke tilladt at s\u00e6tte delm\u00e5lsnotater fremme i tiden.",
+	"error.reason.DraftVisitationAlreadyExistsError": "Der er allerede oprettet en kladde. Der kan kun v\u00e6re en kladde pr. borger."
+};
+export default translations;
